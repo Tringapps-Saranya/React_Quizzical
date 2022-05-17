7bc4d0cf14c151b60a90d133c0396903bdb878c7
Tringapps-Saranya <102800505+Tringapps-Saranya@users.noreply.github.com>
2022-05-17 11:13:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
+import React from 'react'
+import StartQuiz from './components/StartQuiz'
 import Quiz from './components/Quiz';
-
+import {
+  Routes,
+  Route
+} from "react-router-dom";
 function App() {
+
   return (
     <div className="App">
-     <Quiz />
+   
+      <Routes>
+                <Route path = '/' element = {<StartQuiz/>} />
+                <Route path = "/quiz" element = {<Quiz/>} />
+            </Routes>
     </div>
   );
 }
